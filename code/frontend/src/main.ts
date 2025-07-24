fce41401a6b0c9208c37fba94d02d4bec26fcd0d
paul <paulkaisr@gmail.com>
2025-07-24 23:10:32 +0200

    Init Vuetify Frontend

--- code/frontend/src/main.ts
+++ code/frontend/src/main.ts
@@ -1,6 +1,23 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+// Plugins
+import {registerPlugins} from '@/plugins'
+
+// Components
+import App from './App.vue'
+
+// Composables
+import {createApp} from 'vue'
+
+// Styles
+import 'unfonts.css'
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
