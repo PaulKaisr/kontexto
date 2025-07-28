2d7c360c38f8f494a7cb8e4e58f77b0b402948ac
paul <paulkaisr@gmail.com>
2025-07-28 22:15:25 +0200

    feat: implement dynamic coloring

--- code/frontend/src/components/GuessHistory/GuessItem.vue
+++ code/frontend/src/components/GuessHistory/GuessItem.vue
@@ -4,10 +4,18 @@ import {calculateBarFill} from '@/common/maths'
 
 defineProps<{ guess: string, similarity: Similarity | null }>()
 
+const getBarClass = (similarity: Similarity | null) => {
+  if (!similarity || similarity.similarity == null) return 'bg-violet-800';
+  const rank = similarity.similarity;
+  if (rank <= 300) return 'bg-success';
+  if (rank <= 1500) return 'bg-orange-400';
+  return 'bg-red-500';
+}
+
 const getBarFill = (similarity: Similarity | null) => {
-  if (!similarity || similarity.similarity == null) return '0%'
-  const width = calculateBarFill(similarity.similarity)
-  return `${width}%`
+  if (!similarity || similarity.similarity == null) return '0%';
+  const width = calculateBarFill(similarity.similarity);
+  return `${width}%`;
 }
 </script>
 
@@ -19,7 +27,8 @@ const getBarFill = (similarity: Similarity | null) => {
     </div>
     <div
       v-if="similarity"
-      class="bg-amber absolute left-0 bottom-0 h-full rounded transition-all duration-500 -z-10"
+      class="absolute left-0 bottom-0 h-full rounded transition-all duration-500 -z-10"
+      :class="getBarClass(similarity)"
       :style="{ width: getBarFill(similarity) }"
     ></div>
   </v-card>
