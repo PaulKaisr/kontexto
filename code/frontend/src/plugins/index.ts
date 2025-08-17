c82f8845da4ebd0ca22d69084feca45dec0b7b60
paul <paulkaisr@gmail.com>
2025-08-17 19:59:53 +0200

    Formatting

--- code/frontend/src/plugins/index.ts
+++ code/frontend/src/plugins/index.ts
@@ -5,18 +5,18 @@
  */
 
 // Plugins
-import vuetify from './vuetify'
-import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
-import router from '../router'
+import vuetify from "./vuetify";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
+import router from "../router";
 
 // Types
-import type {App} from 'vue'
-import {createPinia} from "pinia";
+import type { App } from "vue";
+import { createPinia } from "pinia";
 
 export function registerPlugins(app: App) {
-  app.use(vuetify)
-  const pinia = createPinia()
-  pinia.use(piniaPluginPersistedstate)
-  app.use(pinia)
-  app.use(router)
+  app.use(vuetify);
+  const pinia = createPinia();
+  pinia.use(piniaPluginPersistedstate);
+  app.use(pinia);
+  app.use(router);
 }
